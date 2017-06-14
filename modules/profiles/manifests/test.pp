8c79bd56c8278d7ddaa0c0d063b16b1bab358435
odumancic <root@puppetslave.mshome.net>
2017-06-13 22:29:38 -0400

    Added comment to test.pp

--- modules/profiles/manifests/test.pp
+++ modules/profiles/manifests/test.pp
@@ -1,20 +1,5 @@
 class profiles::test {
- # commented line 
- # adding new comment
- # let's add something else
 
- ## Paul comment 1
+ # 1st comment - production branch 
 
- # add other commen
- # Oliver adding comment 1
- # added some changes
- # added some changes 2
-
- # Oliver commented on this ...
- # Paul added a comment :)
- # Oliver new comment. ..
- # Paul added a new comment :)
-
- # Oliver added comment 3...
- # Paul added a new comment 3 ...
 }
