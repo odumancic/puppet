58f6e024ce20715478eabc528582b444927b66b5
odumancic <root@puppetslave.mshome.net>
2017-07-12 04:47:16 -0400

    Added some changes on test.pp

--- modules/profiles/manifests/test.pp
+++ modules/profiles/manifests/test.pp
@@ -1,10 +1,4 @@
 class profiles::test {
 
- # 1st comment - production branch 
-
- # 2nd comment - oliver branch
- # 3rd comment - paul branch
-
- # 4th comment - oliver branch
- # 5th comment - paul branch
+  @@file { "/tmp/dbdone.txt": content => "Woo, DB installed!\n", tag => "dbdone", } }
 }
--- modules/profiles/manifests/test1.pp
+++ modules/profiles/manifests/test1.pp
@@ -1,6 +1,9 @@
 class profiles::test1 {
   File <<| tag == dbdone |>>
 
+  notify {"Test":}
+  notify {File<<| tag == dbdone |>>:}
+
 #  Pakackage{'jboss':
 #  require => [ File<<| tag == dbdone |>>], }
 
